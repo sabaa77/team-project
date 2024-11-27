e9c3f887e7f93506b9235b21457a26984b49457e
mo-jallow <momodouj160@gmail.com>
2024-11-27 11:56:30 +0000

    Add files via upload

---
+++ Frontend/search.js
@@ -0,0 +1,12 @@
+    const searchIcon = document.getElementById('search-icon');
+    const searchBar = document.getElementById('search-bar');
+    
+    searchIcon.addEventListener('click', () => {
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+    } else {
+        searchBar.style.display = 'none';
+    }
+        
+    });
+    
