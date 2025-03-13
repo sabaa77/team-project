447e9b5c40971286f16bab4210196af9be847384
sabaa77 <sabaa26@icloud.com>
2025-03-13 15:22:48 +0000

    Update db.php

--- Database/db.php
+++ Database/db.php
@@ -1,10 +1,10 @@
 <?php
-$db_server = "root"
-$db_user = "localhost"
-$db_password = ""; // No space between quotes
+$db_server = "localhost"; 
+$db_user = "root"; 
+$db_password = ""; 
 $db_name = "essencewear";
 
-// Corrected mysqli_connect syntax
+// Create connection
 $conn = new mysqli($db_server, $db_user, $db_password, $db_name);
 
 // Check connection
@@ -14,4 +14,3 @@ if ($conn->connect_error) {
     echo "You are connected!";
 }
 ?>
-
