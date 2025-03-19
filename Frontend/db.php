105a6c7dceb8310dae3d6c697d3fb603fa614a03
mo-jallow <momodouj160@gmail.com>
2025-03-19 10:31:36 +0000

    Update db.php

--- Frontend/db.php
+++ Frontend/db.php
@@ -1,13 +1,11 @@
 <?php
-$db_server = "localhost"; 
+$db_server = "cs2410-web01pvm.aston.ac.uk"; 
 $db_user = "cs2team49"; 
 $db_password = "wHP74YYCEr1LqhK"; 
 $db_name = "cs2team49_db";
 
-// Create connection
 $conn = new mysqli($db_server, $db_user, $db_password, $db_name);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
