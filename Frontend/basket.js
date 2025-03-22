47f7fca2912bfb7134bd93b86a2515ee784bd08d
mo-jallow <momodouj160@gmail.com>
2025-03-22 23:33:28 +0000

    Update basket.js

--- Frontend/basket.js
+++ Frontend/basket.js
@@ -97,7 +97,7 @@ async function updateBackendBasket(basket) {
         const response = await fetch('saveBasket.php', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(basket),
+            body: JSON.stringify(basketObject),
         });
 
         const result = await response.json();
